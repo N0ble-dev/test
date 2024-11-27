c707224fa4383f25d4a2978802cc4c1e448b147b
Nabil Mohamed <106386970+N0ble-dev@users.noreply.github.com>
2024-11-28 00:11:13 +0200

    Delete app directory

--- app/components/MainHeading.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const MainHeading = ({ text, className }: { text: string; className?: string }) => {
-  return <h1 className={`${className || ""} text-3xl font-bold text-black`}>{text}</h1>;
-};
-
-export default MainHeading;
--- app/components/Share.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { FaFacebook, FaInstagram, FaLinkedin } from "react-icons/fa";
-import { FaXTwitter } from "react-icons/fa6";
-const Share = () => {
-  return null
-};
-
-export default Share;
